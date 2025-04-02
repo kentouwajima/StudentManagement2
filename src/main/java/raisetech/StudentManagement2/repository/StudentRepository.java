432fcf73c2583e1fcedac91257d7bb10eea3c493
kentouwajima <u.kento6366@gmail.com>
2025-04-02 13:41:59 +0900

    新規受講生情報を登録する処理を実装

--- src/main/java/raisetech/StudentManagement2/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement2/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package raisetech.StudentManagement2.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement2.data.Student;
 import raisetech.StudentManagement2.data.StudentsCourses;
@@ -14,4 +16,9 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses>searchStudentsCourses();
+
+  @Insert("insert into students(name, kana_name, nickname, email, area, age, sex, remark)"
+      + " values(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudent(Student student);
 }
